1c26e894a563e4cb76600fb92a949b54d85017d9
Blouze <blouze@gmail.com>
2014-05-02 15:40:17 +0200

    Using frequency proportion and added rotational params.

--- harmonograph.js
+++ harmonograph.js
@@ -2,7 +2,7 @@ var harmonograph;
 paper.install(window);
 
 window.onload = function () {
-  paper.setup("myCanvas");
+  paper.setup("paperCanvas");
   harmonograph = new Harmonograph();
   addDatGui();
   harmonograph.render();
@@ -13,37 +13,37 @@ var Harmonograph = function () {
 
   var ref = this;
 
-  this.x = view.center.x, this.y = view.center.y;
-  this.freqX = 1;
-  this.freqS = 1;
-  this.freqY = 1;
-  this.amplitude;
-  this.time, this.tick;
-  this.decay = 1 / 10000;
-  this.timeFactor = 100;
-
-  this.scaleToView = false;
+  this.freqRatio = 1 / 3;
+  this.noise = 10 + (100 - 10) * Math.random();
+  this.rotational = false;
+  this.rotWork = 0.5;
 
   this.render = function () {
 
     project.clear();
 
-    this.phaseX = 2 * Math.PI * Math.random();
-    this.phaseS = 2 * Math.PI * Math.random();
-    this.phaseY = 2 * Math.PI * Math.random();
+    var phaseX = Math.PI / 2;
+    var phaseS = 0;
+    var phaseY = 0;
 
-    this.amplY = view.center.y / 2;
-    this.amplX = this.scaleToView ? view.center.x / 2 : this.amplY;
-    this.amplS = view.center.x / 4;
-    var origAmpl = this.amplX;
+    var amplY = 0.8 * view.center.y;
+    var amplX = 0.6 * view.center.x;
+    var amplS = 0.6 * view.center.x;
+    
+    var origAmpl = amplX;
+    var decay = 1 / 2000;
 
-    this.time = 0;
-    this.tick = 0.15 * Math.min(this.freqX / this.freqY, 1.0);
+    var time = 0;
+    var tick = 0.1;
+    var timePerLoop = 200;
 
-    var noiseFactor = 0.01;
-    this.noiseX = (2 * Math.random() - 1) * noiseFactor;
-    this.noiseS = (2 * Math.random() - 1) * noiseFactor;
-    this.noiseY = (2 * Math.random() - 1) * noiseFactor;
+    var freqS = (!this.rotational ? 2 : 3 / 2) * this.freqRatio;
+    var noiseX = (2 * Math.random() - 1) * this.noise / 10000;
+    //var noiseX = 0;
+    var noiseS = (2 * Math.random() - 1) * this.noise / 10000;
+    //var noiseS = 0;
+    var noiseY = (2 * Math.random() - 1) * this.noise / 10000;
+    //var noiseY = 0;
 
     var segments = [];
 
@@ -52,20 +52,27 @@ var Harmonograph = function () {
 
       with (ref) {
 
-        if (amplX > 1) {
+        if (amplX > 0.1) {
 
-          for (var i = timeFactor - 1; i >= 0; i--) {
+          for (var i = timePerLoop - 1; i >= 0; i--) {
 
             x = view.center.x + 
-            amplX * Math.sin((Math.PI / 6 * freqX + noiseX) * time + phaseX) + 
-            amplS * Math.sin((Math.PI / 6 * freqS + noiseS) * time + phaseS);
+            amplX * Math.sin(phaseX + time * (1 + noiseX)) * (1 - rotWork) + 
+            amplS * Math.sin(phaseS + time * (freqS + noiseS)) * rotWork;
             
-            y = view.center.y + 
-            amplY * Math.sin((Math.PI / 6 * freqY + noiseY) * time + phaseY);
+            if (rotational) {
+              y = view.center.y + 
+              amplY * Math.sin(phaseY + time * (freqRatio + noiseY)) * (1 - rotWork) - 
+              amplS * Math.cos(phaseS + time * (freqS + noiseS)) * rotWork;
+            }
+            else {
+              y = view.center.y + 
+              amplY * Math.sin(phaseY + time * (freqRatio + noiseY));
+            }
             
-            amplX = amplX * (1 - decay);
-            amplY = amplY * (1 - decay);
-            amplS = amplS * (1 - decay);
+            amplX *= (1 - decay * tick);
+            amplY *= (1 - decay * tick);
+            amplS *= (1 - decay * tick);
 
             segments.push([x, y]);
 
@@ -91,110 +98,85 @@ var Harmonograph = function () {
 var addDatGui = function () {
 
   var gui = new dat.GUI({
-    load: {
-      "remembered": {
-        "Unison": {
-          "0": {
-            "freqX": 2,
-            "freqS": 4,
-            "freqY": 2
-          }
-        }, 
-        "Octave": {
-          "0": {
-            "freqX": 2,
-            "freqS": 4,
-            "freqY": 4
-          }
-        }, 
-        "5th": {
-          "0": {
-            "freqX": 4,
-            "freqS": 6,
-            "freqY": 6
-          }
-        }, 
-        "4th": {
-          "0": {
-            "freqX": 6,
-            "freqS": 8,
-            "freqY": 8
-          }
-        }, 
-        "Major 6th": {
-          "0": {
-            "freqX": 3,
-            "freqS": 5,
-            "freqY": 5
-          }
-        }, 
-        "Major 3rd": {
-          "0": {
-            "freqX": 4,
-            "freqS": 5,
-            "freqY": 5
-          }
-        }, 
-        "Minor 3rd": {
-          "0": {
-            "freqX": 5,
-            "freqS": 6,
-            "freqY": 6
-          }
-        }, 
-        "Perfect 11th": {
-          "0": {
-            "freqX": 3,
-            "freqS": 8,
-            "freqY": 8
-          }
-        }, 
-        "7th-limit jazz tuning": {
-          "0": {
-            "freqX": 3,
-            "freqS": 7,
-            "freqY": 7
-          }
-        }, 
-        "random": {
-          "0": {
-            "freqX": Math.floor(Math.random() * 12),
-            "freqS": Math.floor(Math.random() * 12),
-            "freqY": Math.floor(Math.random() * 12)
-          }
-        }
-      },
-      "closed": false,
-      "folders": {
-        "X": {
-          "closed": true,
-        },
-        "Y": {
-          "closed": true,
-        }
-      }
-    }, 
+    load: datJSON, 
     preset: "Unison"
   });
 
-gui.remember(harmonograph);
-gui.add(harmonograph, "freqX", 1, 12).step(1).onChange(function (value) {
-  harmonograph.freqX = value;
-  harmonograph.render();
-});
-gui.add(harmonograph, "freqY", 1, 12).step(1).onChange(function (value) {
-  harmonograph.freqY = value;
-  harmonograph.render();
-});
-gui.add(harmonograph, "freqS", 0, 12).step(1).onChange(function (value) {
-  harmonograph.freqS = value;
-  harmonograph.render();
-});
-gui.add(harmonograph, "scaleToView").onChange(function (value) {
-  harmonograph.scaleToView = value;
-  harmonograph.render();
-});
-gui.add(harmonograph, "render");
+  gui.remember(harmonograph);
+
+  gui.add(harmonograph, "freqRatio", 0.5, 3).onChange(function (value) {
+    harmonograph.freqRatio = value;
+    harmonograph.render();
+  });
+
+  gui.add(harmonograph, "noise", 10, 100).onChange(function (value) {
+    harmonograph.noise = value;
+    harmonograph.render();
+  });
+
+  gui.add(harmonograph, "rotational").onChange(function (value) {
+    harmonograph.rotary = value;
+    harmonograph.render();
+  });
+
+  gui.add(harmonograph, "rotWork", 0.0, 1.0).onChange(function (value) {
+    harmonograph.rotary = value;
+    harmonograph.render();
+  });
+
+  gui.add(harmonograph, "render");
+};
+
+var datJSON = {
+  "remembered": {
+    "Unison": {
+      "0": {
+        "freqRatio": 1
+      }
+    }, 
+    "Octave": {
+      "0": {
+        "freqRatio": 2
+      }
+    }, 
+    "5th": {
+      "0": {
+        "freqRatio": 3 / 2
+      }
+    }, 
+    "4th": {
+      "0": {
+        "freqRatio": 4 / 3
+      }
+    }, 
+    "Major 6th": {
+      "0": {
+        "freqRatio": 5 / 3
+      }
+    }, 
+    "Major 3rd": {
+      "0": {
+        "freqRatio": 5 / 4
+      }
+    }, 
+    "Minor 3rd": {
+      "0": {
+        "freqRatio": 6 / 5
+      }
+    }, 
+    "Perfect 11th": {
+      "0": {
+        "freqRatio": 8 / 3
+      }
+    }, 
+    "7th-limit jazz tuning": {
+      "0": {
+        "freqRatio": 7 / 3
+      }
+    }
+  },
+  "closed": false,
+  "folders": {}
 };
 
 
@@ -212,12 +194,6 @@ if (location.hostname == "localhost") {
       saveAs(blobSVG, curTime+'.svg');
 
       var jsn = {
-        freqX: harmonograph.freqX,
-        freqY: harmonograph.freqY,
-        freqS: harmonograph.freqS,
-        noiseX: harmonograph.noiseX,
-        noiseY: harmonograph.noiseY,
-        noiseS: harmonograph.noiseS
       };
       var blobJSN = new Blob([jsn], {type: "application/json;charset=utf-8"});
       saveAs(blobJSN, curTime+'.txt');
